0f1deec06fbb046fcb42015400572db15050beb5
PierreRoshenko <martik.assassin@gmail.com>
2021-05-07 15:50:47 +0300

    mm

---
+++ src/index.js
@@ -0,0 +1,49 @@
+const hw1 = require('../hw1/martyn_hw_n1-1/main')
+const hw2 = require('../hw2/h2_n2_famuliak_martyn/main')
+const hw3 = require('../hw3/hw3_n3_famuliak_martyn/main')
+const hw4 = require('../hw4/hw4_famuliak_martyn/main')
+const hw5 = require('../hw5/hw5_n5/main')
+const hw6 = require('../hw6/main')
+const hw7 = require('../hw7/hw7/main')
+const hw8 = require('../hw8/hw8/main')
+const hw10 = require('../hw10/hw10/main')
+const hw11 = require('../hw11/hw11/main')
+const hw12 = require('../hw12/hw12/main')
+const hw13 = require('../hw13/hw13/main')
+const hw15 = require('../hw15/hw15/main')
+console.log('hw1:');
+console.log(hw1.cost);
+
+hw2.func()
+
+console.log('hw3:');
+console.log(hw3.convertCurrency('1488uah'));
+
+console.log('hw4:');
+console.log(hw4.getMarksForPairs());
+
+console.log('hw5:');
+console.log(hw5.getModa(1, 5, 8, 6, 7, 58, 2, 4, 9, 7, 5));
+
+console.log('hw6:');
+console.log(hw6.getStudentInfo(hw6.tania));
+console.log(hw6.getStudentInfo(hw6.victor));
+console.log(hw6.getStudentInfo(hw6.anton));
+
+
+console.log('hw7:');
+console.log(hw7.getMySalary(hw7.ukraine));
+console.log(hw7.getMySalary(hw7.latvia));
+console.log(hw7.getMySalary(hw7.litva));
+
+
+console.log('hw8:');
+console.log(hw8.martyn.getMarks);
+
+
+
+hw10.dynamicSqures()
+hw11.sounds()
+
+console.log('hw12:');
+hw12.getChinese(5)
